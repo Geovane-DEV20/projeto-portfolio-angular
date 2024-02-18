f3d5a0dcc22f274fcab4280a684ba0d80b4af041
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-02-18 01:45:59 -0300

    Knowledge component

---
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+import { IKnowLedge } from '../../interface/Knowledge-interface';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+
+  //tipei na classe Knowledge-interface
+  public arrayKnowledge = signal<IKnowLedge[]>([
+    {
+        src: 'assets/icons/knowledge/html5.svg',
+        alt: 'Icone de conhecimento de html5',
+    },
+    {
+        src: 'assets/icons/knowledge/css3.svg',
+        alt: 'Icone de conhecimento de CSS3',
+    },
+    {
+      src: 'assets/icons/knowledge/javascript.svg',
+      alt: 'Icone de conhecimento de html5',
+    },
+    {
+        src: 'assets/icons/knowledge/angular.svg',
+        alt: 'Icone de conhecimento de CSS3',
+    },
+    // Adicione mais objetos conforme necessário
+]);
+
+}
